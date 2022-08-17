9e367772f17a52fe7d8ca357a58920e83948950d
ABSanthosh <a.b.santhosh02@gmail.com>
2022-08-17 13:11:16 +0530

    feat: working flashcard

--- src/Components/FlashCard/FlashCard.tsx
+++ src/Components/FlashCard/FlashCard.tsx
@@ -1,6 +1,69 @@
 import React from "react";
 import "./FlashCard.scss";
 
-export default function FlashCard() {
-  return <div className="FlashCardWrapper">FlashCard</div>;
+interface FlashcardProps {
+  frontHTML: string;
+  frontCardStyle?: React.CSSProperties;
+  frontContentStyle?: React.CSSProperties;
+  backHTML: string;
+  backCardStyle?: React.CSSProperties;
+  backContentStyle?: React.CSSProperties;
+  className?: string;
+  height?: string;
+  width?: string;
+  resetState?: boolean;
+  style?: React.CSSProperties;
+  onCardFlip?: () => void;
+}
+
+export default function Flashcard({
+  frontHTML,
+  frontCardStyle,
+  frontContentStyle,
+  backHTML,
+  backCardStyle,
+  backContentStyle,
+  style,
+  className = "",
+  height,
+  width,
+  resetState = false,
+  onCardFlip,
+}: FlashcardProps) {
+  const [isFlipped, setIsFlipped] = React.useState(false);
+  React.useEffect(() => {
+    setIsFlipped(false);
+  }, [resetState]);
+
+  return (
+    <div
+      className={`FlashcardWrapper ${className}`}
+      style={{ height: height, width: width, ...style }}
+    >
+      <div
+        className={`FlashcardWrapper__item ${
+          isFlipped ? "FlashcardWrapper__item--flip" : ""
+        }`}
+        onClick={() => {
+          setIsFlipped(!isFlipped);
+          onCardFlip(!isFlipped);
+        }}
+      >
+        <div className="FlashcardWrapper__item--front" style={frontCardStyle}>
+          <div
+            className="FlashcardWrapper__item--content"
+            dangerouslySetInnerHTML={{ __html: frontHTML }}
+            style={frontContentStyle}
+          />
+        </div>
+        <div className="FlashcardWrapper__item--back" style={backCardStyle}>
+          <div
+            className="FlashcardWrapper__item--content"
+            dangerouslySetInnerHTML={{ __html: backHTML }}
+            style={backContentStyle}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
